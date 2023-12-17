919571df98ee0d3b6690aa4592c13fa161349b1d
Bushra Zia <bushraz072@gamil.com>
2023-12-17 12:05:43 +0530

    changes

---
+++ sc.js
@@ -0,0 +1,35 @@
+const openModalBtn = document.getElementById("btn_show_modal");
+const modal = document.getElementById("modal_window");
+const closeModalBtn = document.getElementById("modal_close_btn");
+
+function modalPopUp() {
+  if (modal.style.display === "none") {
+    modal.style.display = "block";
+
+    //   backgroundImage.style.opacity = "0.5";
+  } else {
+    modal.style.display = "none";
+    //   backgroundImage.style.opacity = "1";
+  }
+}
+
+openModalBtn.addEventListener("click", modalPopUp);
+closeModalBtn.addEventListener('click', modalPopUp);
+
+
+
+/*
+openModalBtn.addEventListener('click', function(){
+    modal.style.display = "block";
+})
+
+closeModalBtn.addEventListener('click', function(){
+    modal.style.display = "none";
+}) 
+*/
+
+window.addEventListener('click', function(e) {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+})
