014ddeddb67143c5271d623b77b20e031f70f726
Bushra Zia <bushraz072@gmail.com>
2023-12-29 03:12:26 +0530

    zxc

--- sc.js
+++ sc.js
@@ -1,24 +1,45 @@
 //MODAL-OPEN-ACCOUNT WINDOW
+// const modal = document.getElementById("modal_window");
+// const modalBackground = document.querySelector(".modal_background");
+// const openModalBtn = document.getElementById("btn_show_modal");
+// const closeModalBtn = document.getElementById("modal_close_btn");
+
+// function modalPopUp() {
+//   if (modal.style.display === "none") {
+//     modal.style.display = "block";
+//     modalBackground.style.display = "block";
+
+//   } 
+// }
+
+// openModalBtn.addEventListener("click", modalPopUp);
+// closeModalBtn.addEventListener("click", modalPopUp);
+
+// // window.addEventListener('click', function(e){
+// //   if(e.target === modal){
+// //     modal.style.display = "none";
+// //   }
+// // })
+
 const modal = document.getElementById("modal_window");
 const modalBackground = document.querySelector(".modal_background");
 const openModalBtn = document.getElementById("btn_show_modal");
 const closeModalBtn = document.getElementById("modal_close_btn");
 
 function modalPopUp() {
+  console.log("btn is clicked")
   if (modal.style.display === "none") {
     modal.style.display = "block";
     modalBackground.style.display = "block";
-  } 
+  }else{
+    modal.style.display = "none";
+    modalBackground.style.display = "none";
+  }
+
 }
 
 openModalBtn.addEventListener("click", modalPopUp);
 closeModalBtn.addEventListener("click", modalPopUp);
-
-window.addEventListener('click', function(e){
-  if(e.target === modal){
-    modal.style.display = "none";
-  }
-})
 
 //OPERATION-PAGE MODAL
 const operation_modal_btn = document.querySelectorAll(".btns");
@@ -31,6 +52,7 @@ operation_modal_btn.forEach((index) => {
 //testimonial
 let currentIndex = 0;
 const slides = document.querySelectorAll(".test");
+const dots = document.querySelectorAll('.dots');
 
 const totalSlides = slides.length
 function showslide(index) {
@@ -47,3 +69,17 @@ function prevSlide (){
   currentIndex = (currentIndex - 1 +totalSlides) % totalSlides;
   showslide(currentIndex);
 }
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', function(){
+    currentIndex = index;
+    showslide(currentIndex)
+    
+  })
+});
+
+// function updateDots(){
+//   dots.
+// }
+
+
